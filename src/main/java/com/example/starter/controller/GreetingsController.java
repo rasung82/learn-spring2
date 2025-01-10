f94db5d9c995c743d9846e5967590edeb73140b5
rasung.woo <rasung.woo@sk.com>
2025-01-10 23:38:16 +0900

    Add /bye

--- src/main/java/com/example/starter/controller/GreetingsController.java
+++ src/main/java/com/example/starter/controller/GreetingsController.java
@@ -5,11 +5,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class WelcomeController {
+public class GreetingsController {
 
     @GetMapping("/greetings")
     public String niceToMeetYou(Model model) {
         model.addAttribute("userName", "우");
-        return "greetings"; // templates/greeting.mustache
+        return "greetings"; // templates > /greeting.mustache
+    }
+
+    @GetMapping("/bye")
+    public String seeYouNext(Model model) {
+        model.addAttribute("userName", "우");
+        return "goodbye";
+
     }
 }
